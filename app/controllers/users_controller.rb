56c9445b24a177c8ccb9dce1bf7a4d8d5f601f6d
EC2 Default User <ec2-user@ip-172-31-47-246.ap-northeast-1.compute.internal>
2020-02-04 14:24:21 +0000

    0204 taskpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
+  
   private
   
     def user_params
